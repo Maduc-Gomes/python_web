0caff57396e7ca9a8d2eed8bb0395b02493c04a8
M. Eduarda Gomes <eduarda.cidade.g@gmail.com>
2023-02-15 23:06:53 +0000

    aula 3, parte 1

--- app.py
+++ app.py
@@ -1,23 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 app = Flask(__name__)
+SECRET_KEY = "pudim"
+app.config.from_object(__name__)
 
 # POSTS MOCK
 posts = [
-{
-"titulo": "Post 1",
-"texto": "Meu primeiro post"
-},
-{
-"titulo": "Post 2",
-"texto": "olha eu aqui de novo"
-}
+    {
+        "titulo": "Post 1",
+        "texto": "Meu primeiro Post"
+    },
+    {
+        "titulo": "Post 2",
+        "texto": "Olha eu aqui de novo"
+    },
+       {
+        "titulo": "Post 3",
+        "texto": "Novo Post"
+    }
 ]
+# USER MOCKS
+USERNAME = "admin"
+PASSWORD = "admin"
 
 @app.route('/')
 def exibir_entradas():
-    
-    return render_template("layout.html", entradas=posts)
+    return render_template("exibir_entradas.html", entradas=posts)
 
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    erro = ""
+    if request.method == "POST":
+        if request.form['username'] == USERNAME and request.form['password'] == PASSWORD:
+            session['logado'] = True
+            flash("login efetuado com sucesso")
+            return redirect(url_for('exibir_entradas'))
+        erro = "Usuário ou senha inválidos"
+    return render_template("login.html", erro=erro)
+
+@app.route('/logout')
+def logout():
+    session.pop('logado', None)
+    flash('Logout realizado com sucesso')
+    return redirect(url_for('exibir_entradas'))
 
     
